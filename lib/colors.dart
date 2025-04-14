1235d29b08d468c092488ce49d85fbc9508e2760
RADO-DAYEF\Rado Dayef <mouraddayef9@gmailcom>
2025-04-14 11:28:08 -0700

    last update

---
+++ lib/colors.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+class AppColors{
+  static Color darkBlue = Color(0xFF244463);
+  static Color black = Color(0xFF000000);
+}
--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_sec_2/colors.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -6,12 +7,18 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.lightBlue,
+      backgroundColor: AppColors.darkBlue,
       appBar: AppBar(
-        backgroundColor: Colors.lightBlue,
+        backgroundColor: AppColors.darkBlue,
         foregroundColor: Colors.black,
         centerTitle: true,
-        title: Text("Business Card",style: TextStyle(fontWeight: FontWeight.bold,fontSize: 25),),
+        title: Text(
+          "Business Card",
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+            fontSize: 25,
+          ),
+        ),
       ),
       body: Center(
         child: Column(
@@ -22,12 +29,79 @@ class HomeScreen extends StatelessWidget {
               backgroundColor: Colors.white,
               child: Image.network("https://cdn-icons-png.flaticon.com/128/1864/1864521.png"),
             ),
-            SizedBox(height: 20,),
-            Text("Mahmoud Talal",style: TextStyle(color: Colors.black, fontWeight: FontWeight.bold, fontSize: 30),),
-            SizedBox(height: 5,),
-            Text("Flutter Dev",style: TextStyle(color: Colors.black, fontWeight: FontWeight.bold, fontSize: 30, fontFamily: "Times"),),
-            Divider(color: Colors.grey, thickness: 2, indent: 20, endIndent: 20,),
-
+            SizedBox(
+              height: 20,
+            ),
+            Text(
+              "Mahmoud Talal",
+              style: TextStyle(
+                fontSize: 30,
+                color: Colors.white,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(
+              height: 5,
+            ),
+            Text(
+              "Flutter Dev",
+              style: TextStyle(
+                color: Colors.white,
+                fontWeight: FontWeight.bold,
+                fontSize: 30,
+                fontFamily: "Times",
+              ),
+            ),
+            Divider(
+              indent: 20,
+              thickness: 2,
+              endIndent: 20,
+              color: Colors.grey,
+            ),
+            SizedBox(
+              height: 25,
+            ),
+            Container(
+              padding: EdgeInsets.all(5),
+              margin: EdgeInsets.symmetric(horizontal: 30),
+              decoration: BoxDecoration(
+                color: Colors.white,
+              ),
+              child: Row(
+                children: [
+                  Expanded(
+                    flex:0,
+                    child: Icon(Icons.phone),
+                  ),
+                  Expanded(
+                    flex:1,
+                    child: Center(child: Text("+20 100 084 5943")),
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(
+              height: 25,
+            ),
+            Container(
+              padding: EdgeInsets.all(5),
+              margin: EdgeInsets.symmetric(horizontal: 30),
+              decoration: BoxDecoration(
+                color: Colors.white,
+              ),
+              child: Row(
+                children: [
+                  Expanded(
+                    flex:0,
+                    child: Icon(Icons.phone),
+                  ),
+                  Expanded(
+                    flex:1,
+                    child: Center(child: Text("+20 100 084 5943")),
+                  ),
+                ],
+              ),
+            ),
           ],
         ),
       ),
